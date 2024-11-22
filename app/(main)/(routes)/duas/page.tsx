0d0171163bc8fe49896f72aafa63745584485a09
Imran <md.imransyam1956@gmail.com>
2024-11-23 05:55:43 +0600

    debug13

--- app/(main)/(routes)/duas/page.tsx
+++ app/(main)/(routes)/duas/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import DualistSkeleton from "./[categoryName]/_components/dua/dualist-skeleton";
 
 const DuasPage = () => {
-  const categoryId = useSearchParams().get('cat');
+    // const fullUrl = typeof window !== "undefined" ? window.location.href : "";
+
+    // console.log(fullUrl)
+//   const categoryId = useSearchParams().get('cat');
 //   console.log(categoryId);
-  if(categoryId == null){
     return (
         <DualistSkeleton />
     )
-  }
+//   }
 //   return;
 }
 
