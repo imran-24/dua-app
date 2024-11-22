08dc3e725846007325266ec8a6c679d3be3ede03
Imran <md.imransyam1956@gmail.com>
2024-11-23 04:41:28 +0600

    debug11

--- app/(main)/(routes)/duas/page.tsx
+++ app/(main)/(routes)/duas/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-// import { useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import DualistSkeleton from "./[categoryName]/_components/dua/dualist-skeleton";
 
 const DuasPage = () => {
-//   const categoryId = useSearchParams().get('cat');
+  const categoryId = useSearchParams().get('cat');
 //   console.log(categoryId);
-//   if(categoryId == null){
+  if(categoryId == null){
     return (
         <DualistSkeleton />
     )
-//   }
+  }
 //   return;
 }
 
