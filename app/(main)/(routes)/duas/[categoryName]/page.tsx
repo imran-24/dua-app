b86dd822f0f46331cf9efbbd893b9bb29739ccd0
Imran <md.imransyam1956@gmail.com>
2024-11-23 05:38:18 +0600

    debug13

--- app/(main)/(routes)/duas/[categoryName]/page.tsx
+++ app/(main)/(routes)/duas/[categoryName]/page.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
 // import DuaList from "./_components/dua/dualist";
 // import { getDuasByCategoryId } from "@/actions/queries";
 import Settings from "@/components/settings/settings";
 
-
-const DuaPage = async ({ searchParams }: { searchParams: Promise<{ cat: string }> }) => {
+const DuaPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ cat: string }>;
+}) => {
   // const categories = getCategoriesWithSubCategories();
-  const categoryId = ((await searchParams).cat); // Resolve the promise
+  const categoryId = (await searchParams).cat; // Resolve the promise
 
   console.log(searchParams, categoryId);
 
   // const duas = getDuasByCategoryId(categoryId);
   return (
     <div className='w-full grid grid-cols-8 gap-x-4'>
-      {/* <DuaList duas={duas} categories={categories}  /> */}
-      Hi there
-      <Settings />
+      <Suspense>
+        {/* <DuaList duas={duas} categories={categories}  /> */}
+        Hi there
+        <Settings />
+      </Suspense>
     </div>
   );
 };
