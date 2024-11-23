735d5b4eeab148617d15dbf8d675273bafe4794c
Imran <md.imransyam1956@gmail.com>
2024-11-23 10:31:57 +0600

    dua page

--- components/page.tsx
+++ components/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // import { useSearchParams } from "next/navigation";
-import DualistSkeleton from "./[categoryName]/_components/dua/dualist-skeleton";
+import DualistSkeleton from "../app/(main)/(routes)/duas/[categoryName]/_components/dua/dualist-skeleton";
 
 const DuasPage = () => {
   //   const categoryId = useSearchParams().get('cat');
